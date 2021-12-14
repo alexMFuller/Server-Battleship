19e412f275ebd95b67c21feaa8820c75b42e62ab
Alex Fuller <81948782+alexMFuller@users.noreply.github.com>
2021-12-14 15:15:02 +0000

    Initial commit

---
+++ CMakeFiles/battleship.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/runner/Battleship/src/game.c" "/home/runner/Battleship/CMakeFiles/battleship.dir/src/game.c.o"
+  "/home/runner/Battleship/src/io.c" "/home/runner/Battleship/CMakeFiles/battleship.dir/src/io.c.o"
+  "/home/runner/Battleship/src/main.c" "/home/runner/Battleship/CMakeFiles/battleship.dir/src/main.c.o"
+  "/home/runner/Battleship/src/record.c" "/home/runner/Battleship/CMakeFiles/battleship.dir/src/record.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/battleship.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/battleship.dir/src/game.c.o"
+  "CMakeFiles/battleship.dir/src/io.c.o"
+  "CMakeFiles/battleship.dir/src/main.c.o"
+  "CMakeFiles/battleship.dir/src/record.c.o"
+  "battleship.pdb"
+  "battleship"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
